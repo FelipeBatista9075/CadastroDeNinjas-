fde90f663f02f0cc59dd1f1012d4eb090f08e1c9
FelipeBatista9075 <felipebdias248@gmail.com>
2025-03-15 00:04:39 -0300

    feat: Adiciona os ControllersUi para a utilizacao do thymeleaf de forma renderizada

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Contoller/MissoesControllerUi.java
@@ -0,0 +1,29 @@
+package dev.java10x.CadastroDeNinjas.Missoes.Contoller;
+
+import dev.java10x.CadastroDeNinjas.Missoes.DTO.MissoesDTO;
+import dev.java10x.CadastroDeNinjas.Missoes.Service.MissoesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("missoes/ui")
+public class MissoesControllerUi {
+
+    private final MissoesService missoesService;
+
+    public MissoesControllerUi(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
+    @GetMapping("/listar")
+    public String listarMissoes(Model model){
+        List<MissoesDTO> missoes = missoesService.listarMissoes();
+        model.addAttribute("missoes", missoes);
+        return "listarMissoes";
+    }
+
+}
